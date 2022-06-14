7b946e7b7d4deb604ef6bac2a55827fe09987c03
Cassio Oliveira Silva <cos@vortx.com.br>
2022-06-14 10:30:34 -0300

    first commit

---
+++ migrations/20220614132035-create-user.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING(100),
+        unique: true
+      }
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('users');
+  }
+};
